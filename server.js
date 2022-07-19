ee976714bcec06f04ad1e788c95baa9da63e58d6
superstar <83311833+clever-web@users.noreply.github.com>
2022-07-19 20:45:48 +0300

    add body-parser and mongodb

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
 console.log('May Node be with you')
 
-const express = require('express');
-const app = express();
+const express = require('express')
+const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient
+const app = express()
 
+app.listen(3000, function() {
+    console.log('listening on 3000')
+})
+
+// Make sure you place body-parser before your CRUD handlers!
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.post('/quotes', (req, res) => {
+    console.log(req.body)
+})
