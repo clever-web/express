f6f1f5cae0398cb5acfe10673db86f725319396b
superstar <83311833+clever-web@users.noreply.github.com>
2022-07-26 08:48:17 +0300

    connect to database

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-console.log('May Node be with you')
-
 const express = require('express')
 const bodyParser = require('body-parser')
 const MongoClient = require('mongodb').MongoClient
@@ -9,13 +7,31 @@ app.listen(3000, function() {
     console.log('listening on 3000')
 })
 
-// Make sure you place body-parser before your CRUD handlers!
-app.use(bodyParser.urlencoded({ extended: true }))
+connectionString = 'mongodb+srv://destiny:destiny98@cluster0.ip5jqg9.mongodb.net/?retryWrites=true&w=majority';
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
+MongoClient.connect(connectionString, { useUnifiedTopology: true })
+  .then(client => {
+    console.log('Connected to Database')
+    const db = client.db('star-wars-quotes')
+    const quotesCollection = db.collection('quotes')
+
+    // Make sure you place body-parser before your CRUD handlers!
+    app.use(bodyParser.urlencoded({ extended: true }))
+
+    app.get('/', (req, res) => {
+        res.sendFile(__dirname + '/index.html')
+    })
+
+    app.post('/quotes', (req, res) => {
+        quotesCollection.insertOne(req.body)
+        .then(result => {
+            res.redirect('/')
+        })
+        .catch(error => console.error(error))
+        // console.log(req.body)
+    })
 })
 
-app.post('/quotes', (req, res) => {
-    console.log(req.body)
-})
+
+
+
